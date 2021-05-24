02985399285813b3e0bd18fad882051fdefbe740
AlexL70 <alexander.levinson.70@gmail.com>
2021-05-24 14:32:14 +0300

    2. Passing Various Kinds Of Parameters

--- TextService/TextService/IMyService.cs
+++ TextService/TextService/IMyService.cs
@@ -7,5 +7,17 @@ namespace TextService
     {
         [OperationContract]
         string GetData();
+
+        [OperationContract]
+        string GetMessage(string Name);
+
+        [OperationContract]
+        string GetResult(int Sid, string SName, int M1, int M2, int M3);
+
+        [OperationContract]
+        int GetMax(int[] arr);
+
+        [OperationContract]
+        int[] GetSorted(int[] arr);
     }
 }
--- TextService/TextService/MyService.cs
+++ TextService/TextService/MyService.cs
@@ -1,7 +1,32 @@
-﻿namespace TextService
+﻿using System;
+using System.Threading;
+
+namespace TextService
 {
     public class MyService : IMyService
     {
         public string GetData() => "My service string";
+
+        public string GetMessage(string Name) => $"Hello Mr/Ms {Name}";
+
+        public string GetResult(int Sid, string SName, int M1, int M2, int M3)
+        {
+            var avg = (M1 + M2 + M3) / 3.0;
+            return avg >= 35.0 ? "Pass" : "Fail";
+        }
+
+        public int GetMax(int[] arr)
+        {
+            var max = arr[0];
+            foreach(var x in arr)
+                max = x > max ? x : max;
+            return max;
+        }
+
+        public int[] GetSorted(int[] arr)
+        {
+            Array.Sort(arr);
+            return arr;
+        }
     }
 }
