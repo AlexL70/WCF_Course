4dc4e761cd83d1f1ddab39b196f7abce8ad77262
AlexL70 <alexander.levinson.70@gmail.com>
2021-05-24 20:35:58 +0300

    6. Creating A Client

---
+++ TextService/MyClient/Default.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using MyClient.MyServiceReference;
+
+namespace MyClient
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            MyServiceClient client = new MyServiceClient();
+            DropDownList1.DataSource = client.GetAllCountries().ToList();
+            DropDownList1.DataValueField = nameof(Country.Id);
+            DropDownList1.DataTextField = nameof(Country.Name);
+            DropDownList1.DataBind();
+        }
+    }
+}
