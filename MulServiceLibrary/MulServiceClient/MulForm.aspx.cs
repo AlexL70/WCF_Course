331b180aa8111e96530763398acf97aaf2b327e4
AlexL70 <alexander.levinson.70@gmail.com>
2021-05-25 20:09:29 +0300

    11. End Point Implementation - II

---
+++ MulServiceLibrary/MulServiceClient/MulForm.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MulServiceClient
+{
+    public partial class MulForm : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            MulServiceReference.MulServiceClient client = new MulServiceReference.MulServiceClient("BasicHttpBinding_IMulService");
+            Response.Write(client.Mult(5, 5));
+        }
+    }
+}
