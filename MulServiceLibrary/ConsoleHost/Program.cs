b18baad059d302fc9f720e99a41b25089057fae8
AlexL70 <alexander.levinson.70@gmail.com>
2021-05-26 21:05:21 +0300

    15. Multiple Endpoints With Different Bindings

--- MulServiceLibrary/ConsoleHost/Program.cs
+++ MulServiceLibrary/ConsoleHost/Program.cs
@@ -9,14 +9,18 @@ namespace ConsoleHost
     {
         static void Main(string[] args)
         {
-            var baseAddress = new Uri("http://localhost:63635");
-            var host = new ServiceHost(typeof(MulService), baseAddress);
-            var endPoint = host.AddServiceEndpoint(typeof(IMulService), new BasicHttpBinding(), baseAddress);
+            var tcpBaseAddress = new Uri("net.tcp://localhost:63690");
+            var httpBaseAddress = new Uri("http://localnost:63635");
+            var host = new ServiceHost(typeof(MulService), httpBaseAddress /*, tcpBaseAddress*/);
+            var tcpEndPoint = host.AddServiceEndpoint(typeof(IMulService), new NetTcpBinding(), tcpBaseAddress);
+            var httpEndPoint = host.AddServiceEndpoint(typeof(IMulService), new BasicHttpBinding(), httpBaseAddress);
             var metadataBehavior = new ServiceMetadataBehavior() { HttpGetEnabled = true };
             host.Description.Behaviors.Add(metadataBehavior);
             host.Open();
 
-            Console.WriteLine($"Service started at {baseAddress.AbsoluteUri}. Press \"Enter\" to stop it.");
+            Console.WriteLine($"Service started at {host.BaseAddresses[0]}. Press \"Enter\" to stop it.");
+            foreach(var ep in host.Description.Endpoints)
+                Console.WriteLine($"Endpoint description: {Environment.NewLine}    Address: {ep.Address}{Environment.NewLine}    Binding: {ep.Binding.Name}{Environment.NewLine}    Contract: {ep.Contract.ContractType}");
             Console.ReadLine();
             host.Close();
             Console.WriteLine($"Stopped.");
